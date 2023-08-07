e2b19676e25838ed4580379812d40feaf436be05
Callum Shervill <shervc272@gmail.com>
2023-08-07 14:21:45 +0100

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	//Script
+</script>
+
+<svelte:head>
+	<title>CampusGuessr</title>
+</svelte:head>
+
+<section>
+	<h1>
+		CampusGuessr
+	</h1>
+
+</section>
+
+<style>
+	/* Style */
+</style>
